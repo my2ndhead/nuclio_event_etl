73fbb1dbd1c09e2d1e9d0374cbcfee738c34a209
my2ndhead <johndoe@example.com>
2018-02-27 22:18:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Experimental Nuclio Event ETL Functions
+- ** Author **:		Mika Borner <mika.borner@gmail.com>
+- ** Description**:	ETL Functions for Extracting, Loading and Transforming timeseries events
+- ** Version **:	@build.version@
+
+# Functions
+
+## fieldextraction
+
+- Header: X-Regex: <regex>
+- Body: String
+- Response: JSON with fields
+
+- Example:
+
+$ curl localhost:42314/ \
+  -H "X-Regex: Name=(?P<field1>\w+).*?Firstname=(?P<field2>\w+)" \
+  -d "Name=Kent Firstname=Clark" \
+  -vvvv
+
+   Trying 127.0.0.1...
+* Connected to localhost (127.0.0.1) port 42314 (#0)
+> POST / HTTP/1.1
+> Host: localhost:42314
+> User-Agent: curl/7.47.0
+> Accept: */*
+> X-Regex: Name=(?P<field1>\w+).*?Firstname=(?P<field2>\w+)
+> Content-Length: 25
+> Content-Type: application/x-www-form-urlencoded
+> 
+* upload completely sent off: 25 out of 25 bytes
+< HTTP/1.1 200 OK
+< Server: nuclio
+< Date: Tue, 27 Feb 2018 15:10:43 GMT
+< Content-Type: application/json
+< Content-Length: 34
+< 
+* Connection #0 to host localhost left intact
+{"field1":"Kent","field2":"Clark"}
+
+
---
+++ fieldextractor.go
@@ -0,0 +1,76 @@
+package splunk_etl
+
+import (
+    "encoding/json"
+    "regexp"
+    "github.com/nuclio/nuclio-sdk-go"
+)
+
+func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
+
+	// Unstructured debug message
+	//context.Logger.Debug("Process document %s, length %d", event.GetPath(), event.GetSize())
+
+    body := string(event.GetBody())
+
+    
+    if len(body) == 0 {
+        context.Logger.Debug("Body empty")
+        return nuclio.Response{
+            StatusCode:  204,
+            ContentType: "application/text",
+            
+            Body: []byte("Body empty"),
+         }, nil
+    }
+
+    myRegexPattern := string(event.GetHeader("X-Regex").([]byte))
+    context.Logger.Info(myRegexPattern)
+    r, err := regexp.Compile(myRegexPattern)
+
+    if err !=  nil {
+        return nuclio.Response{
+            StatusCode:  400,
+            ContentType: "application/text",
+            Body: []byte("Regex error"),
+         }, nil
+    }
+    
+    fields := reSubMatchMap(r, body)
+    
+	if fields != nil { 
+        // Format into JSON 
+ 		fieldsJson, _ := json.Marshal(fields) 	
+	    return nuclio.Response{
+	           StatusCode: 200,
+	           ContentType: "application/json",
+	           Body: []byte(fieldsJson),
+	    }, nil
+ 	} else {
+	    // Catch non matches and return 204
+        context.Logger.Debug("Body empty")
+        return nuclio.Response{
+            StatusCode:  204,
+            ContentType: "application/text",
+            Body: []byte("Body empty"),
+         }, nil
+	}
+}
+
+func reSubMatchMap(r *regexp.Regexp, str string) (map[string]string) {
+    
+    match := r.FindStringSubmatch(str)
+
+    if match != nil {
+   		subMatchMap := make(map[string]string)
+    	for i, name := range r.SubexpNames() {
+        	if i != 0 {
+            	subMatchMap[name] = match[i]
+        	}
+    	} 	
+	return subMatchMap
+     
+    } else {
+    	return nil
+    }
+}
