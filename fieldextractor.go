ded6a1a70270a52f30bb42cf63041ebb150f5c25
my2ndhead <johndoe@example.com>
2018-03-03 12:04:48 +0100

    Small fixes

--- fieldextractor.go
+++ fieldextractor.go
@@ -1,76 +1,75 @@
 package main
 
 import (
-    "encoding/json"
-    "regexp"
-    "github.com/nuclio/nuclio-sdk-go"
+	"encoding/json"
+	"regexp"
+
+	"github.com/nuclio/nuclio-sdk-go"
 )
 
+// Handler for HTTP Triggers
 func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 
 	// Unstructured debug message
 	//context.Logger.Debug("Process document %s, length %d", event.GetPath(), event.GetSize())
 
-    body := string(event.GetBody())
+	body := string(event.GetBody())
 
-    
-    if len(body) == 0 {
-        context.Logger.Debug("Body empty")
-        return nuclio.Response{
-            StatusCode:  204,
-            ContentType: "application/text",
-            
-            Body: []byte("Body empty"),
-         }, nil
-    }
+	if len(body) == 0 {
+		context.Logger.Debug("Body empty")
+		return nuclio.Response{
+			StatusCode:  204,
+			ContentType: "application/text",
 
-    myRegexPattern := string(event.GetHeader("X-Regex").([]byte))
-    context.Logger.Info(myRegexPattern)
-    r, err := regexp.Compile(myRegexPattern)
-
-    if err !=  nil {
-        return nuclio.Response{
-            StatusCode:  400,
-            ContentType: "application/text",
-            Body: []byte("Regex error"),
-         }, nil
-    }
-    
-    fields := reSubMatchMap(r, body)
-    
-	if fields != nil { 
-        // Format into JSON 
- 		fieldsJson, _ := json.Marshal(fields) 	
-	    return nuclio.Response{
-	           StatusCode: 200,
-	           ContentType: "application/json",
-	           Body: []byte(fieldsJson),
-	    }, nil
- 	} else {
-	    // Catch non matches and return 204
-        context.Logger.Debug("Body empty")
-        return nuclio.Response{
-            StatusCode:  204,
-            ContentType: "application/text",
-            Body: []byte("Body empty"),
-         }, nil
+			Body: []byte("Body empty"),
+		}, nil
 	}
+
+	myRegexPattern := string(event.GetHeader("X-Regex").([]byte))
+	context.Logger.Info(myRegexPattern)
+	r, err := regexp.Compile(myRegexPattern)
+
+	if err != nil {
+		return nuclio.Response{
+			StatusCode:  400,
+			ContentType: "application/text",
+			Body:        []byte("Regex error"),
+		}, nil
+	}
+
+	fields := reSubMatchMap(r, body)
+
+	if fields != nil {
+		// Format into JSON
+		JSON, _ := json.Marshal(fields)
+		return nuclio.Response{
+			StatusCode:  200,
+			ContentType: "application/json",
+			Body:        []byte(fieldsJSON),
+		}, nil
+	}
+	// Catch non matches and return 204
+	context.Logger.Debug("Body empty")
+	return nuclio.Response{
+		StatusCode:  204,
+		ContentType: "application/text",
+		Body:        []byte("Body empty"),
+	}, nil
 }
 
-func reSubMatchMap(r *regexp.Regexp, str string) (map[string]string) {
-    
-    match := r.FindStringSubmatch(str)
+func reSubMatchMap(r *regexp.Regexp, str string) map[string]string {
 
-    if match != nil {
-   		subMatchMap := make(map[string]string)
-    	for i, name := range r.SubexpNames() {
-        	if i != 0 {
-            	subMatchMap[name] = match[i]
-        	}
-    	} 	
-	return subMatchMap
-     
-    } else {
-    	return nil
-    }
+	match := r.FindStringSubmatch(str)
+
+	if match != nil {
+		subMatchMap := make(map[string]string)
+		for i, name := range r.SubexpNames() {
+			if i != 0 {
+				subMatchMap[name] = match[i]
+			}
+		}
+		return subMatchMap
+
+	}
+	return nil
 }
